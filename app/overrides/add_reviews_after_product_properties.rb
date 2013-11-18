7760a803f8b8da1505d5e093f148fec6ef01afba
yakjuly <yakjuly@gmail.com>
2013-11-18 17:53:16 +0800

    change hook place

--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_properties_767643482",
-                     :insert_after => "[data-hook='product_properties']",
+                     :insert_after => "[data-hook='review']",
                      :partial => "spree/shared/reviews",
                      :disabled => false)
