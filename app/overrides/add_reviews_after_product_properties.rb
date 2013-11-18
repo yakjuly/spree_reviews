75b7529e92bf2c581bd15e1122e671298c55b9ef
yakjuly <yakjuly@gmail.com>
2013-11-18 17:54:57 +0800

    --amend

--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_properties_767643482",
-                     :insert_after => "[data-hook='review']",
+                     :insert_top => "[data-hook='review']",
                      :partial => "spree/shared/reviews",
                      :disabled => false)
